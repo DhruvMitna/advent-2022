2fa219f6cf55564a88cf22f88419f4a2c05a86c4
DhruvMitna19 <dhruvmitna9@gmail.com>
2022-12-09 12:49:22 +0530

    day 1

---
+++ 1/1.py
@@ -0,0 +1,29 @@
+with open("1.txt", "r") as Input:
+
+    lines = Input.readlines()
+
+elf = []
+elves = []
+
+for calorie in lines:
+
+    if calorie == "\n":
+
+        elves.append(elf)
+        elf = []
+
+    else:
+
+        elf.append(int(calorie))
+
+Input.close()
+
+elfCalories = [sum(elf) for elf in elves]
+top = []
+
+for _ in range(3):
+
+    top.append(max(elfCalories))
+    elfCalories.remove(max(elfCalories))
+
+print(sum(top))
